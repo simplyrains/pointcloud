34be322e75ebd8a47233c156653cfd0834659fd6
Sarin Achawaranont <sarim.achawaranont@gmail.com>
2015-03-18 18:43:20 +0700

    create image_holder class

---
+++ image_holder.cpp
@@ -0,0 +1,9 @@
+//
+//  image_holder.cpp
+//  pointcloud
+//
+//  Created by Sarin Achawaranont on 3/18/2558 BE.
+//  Copyright (c) 2558 Sarin Achawaranont. All rights reserved.
+//
+
+#include "image_holder.h"
